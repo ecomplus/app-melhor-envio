a8197e16155e56d10a5dce121cbdaa5669c89455
Talisson Trindade <talissonf@gmail.com>
2018-10-16 00:53:14 -0300

    Update: sql prototypes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const qs = require('querystring')
-const sqlite = require('./service/sql')
+const dao = require('./service/sql')
 const port = process.env.PORT || 3000
-const fs = require('fs')
 
 http.createServer(function (request, response) {
 
@@ -37,21 +36,46 @@ let redirect = (request, response) => {
 let callback = (request, response) => {
 
   if (request.method === 'POST') {
-    let body = ''
-    request.on('data', (data) => {
-      body += data
-      //console.log(body)
-    })
-    request.on('end', () => {
-      //sqlite.create()
-      sqlite.postData(body)
+    let requestBody = ''
 
+    request.on('data', (data) => {
+      requestBody += data
+    })
+
+    request.on('end', () => {
+      requestBody = JSON.parse(requestBody)
+
+      dao.select({ application_app_id: requestBody.application.app_id }, (res) => {
+        if (typeof res === 'undefined') {
+          let params = {
+            application_id: requestBody.application._id,
+            application_app_id: requestBody.application.app_id,
+            application_title: requestBody.application.title,
+            authentication_id: requestBody.authentication._id,
+            authentication_permission: JSON.stringify(requestBody.authentication.permissions),
+            store_id: requestBody.store_id
+          }
+          dao.insert(params)
+        } else {
+          let params = {
+            application_id: requestBody.application._id ? requestBody.application._id : res.application_id,
+            application_app_id: requestBody.application.app_id ? requestBody.application.app_id : res.application_app_id,
+            application_title: requestBody.application.title ? requestBody.application.title : res.application_title,
+            authentication_id: requestBody.authentication._id ? requestBody.authentication._id : res.authentication_id,
+            authentication_permission: JSON.stringify(requestBody.authentication.permissions) ? JSON.stringify(requestBody.authentication.permissions) : res.authentication_permission,
+            store_id: requestBody.store_id ? requestBody.store_id : res.store_id
+          }
+
+          dao.update(params, { application_app_id: requestBody.application.app_id })
+        }
+      });
     })
 
   } else {
-    response.write('GET')
-    response.end()
+
   }
+
   response.write('Saved')
   response.end()
 }
+
--- service/sql.js
+++ service/sql.js
@@ -1,62 +1,95 @@
 const sqlite = require('sqlite3').verbose();
 const path = require('path');
 const dbPath = path.resolve(__dirname, 'me.db')
+const TABLE = 'auth_callback'
 
 const db = new sqlite.Database(dbPath, (err) => {
   if (err) {
     return console.error(err.message);
   }
-  console.log('Connected to the in-memory SQlite database.');
 });
 
-function getData(id) {
-  let query = `SELECT * FROM store_tokens WHERE store_id = ?`;
-  db.get(query, id, (err, row) => {
+function insert(data) {
+  let keys = [];
+  let values = []
+  let binds = [];
+
+  for (key in data) {
+    if (!data.hasOwnProperty(key)) continue
+    keys.push(key)
+    values.push(data[key])
+    binds.push('?')
+  }
+
+  let query = 'INSERT INTO ' + TABLE + ' (' + keys.join(',') + ') VALUES (' + binds.join(',') + ')'
+  db.run(query, values, function (err) {
     if (err) {
-      return console.error(err.message);
+      return console.error(err.message)
     }
-    return row ? row : false;
-  });
-  db.close();
+    console.log(`Rows inserted ${this.changes}`)
+    return this.changes
+  })
 }
 
-function postData(data) {
-  let query = `INSERT INTO auth_callback (application_id, application_app_id, application_title, authentication_id, authentication_permission, store_id) VALUES (?,?,?,?,?,?)`;
-  
-  data = JSON.parse(data)
-  
-  let params = [
-      data.application._id,
-      data.application.app_id,
-      data.application.title,
-      data.authentication._id,
-      JSON.stringify(data.authentication.permissions),
-      data.store_id
-  ];
+function select(data, callback) {
+  let key, value
+  for (const index in data) {
+    if (data.hasOwnProperty(index)) {
+      key = index
+      value = data[index]
+    }
+  }
 
-  db.run(query, params, function (err) {
-      console.log(this.changes)
+  let query = 'SELECT * FROM ' + TABLE + ' WHERE ' + key + ' = ?'
+  
+  db.get(query, value, (err, row) => {
     if (err) {
-      return console.error(err.message);
+      return console.error(err.message)
     }
-    console.log(`Rows inserted ${this.changes}`);
-  });
-  // close the database connection
-  db.close();
+    
+    if (callback) {
+      callback(row)
+      return this
+    }
+
+    return row
+  })
 }
 
-function putData(){
+function update(data, clause) {
+  let sets = [];
+  let where = [];
+  let values = [];
+  for (key in data) {
+    if (!data.hasOwnProperty(key)) continue;
+    sets.push(key + ' = ?');
+    values.push(data[key]);
+  }
+  for (key in clause) {
+    if (!clause.hasOwnProperty(key)) continue;
+    where.push(key + ' = ?');
+    values.push(clause[key]);
+  }
 
+  let query = 'UPDATE ' + TABLE + ' SET ' + sets.join(', ') + (where.length > 0 ? ' WHERE ' + where.join(' AND ') : '')
+
+  db.run(query, values, function (err) {
+    if (err) {
+      return console.error(err.message)
+    }
+    console.log(`Rows updated ${this.changes}`)
+    return this.changes
+  })
 }
 
-function create(){
-    sql = `CREATE TABLE auth_callback ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, date DATETIME NOT NULL DEFAULT current_timestamp, application_id INTEGER NOT NULL, application_app_id INTEGER NOT NULL, application_title VARCHAR NOT NULL, authentication_id INTEGER NOT NULL, authentication_permission TEXT NOT NULL, me_refresh_token TEXT NOT NULL )`
-    db.run(sql);
+function create() {
+  sql = `CREATE TABLE auth_callback ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, date DATETIME NOT NULL DEFAULT current_timestamp, application_id INTEGER NOT NULL, application_app_id INTEGER NOT NULL, application_title letCHAR NOT NULL, authentication_id INTEGER NOT NULL, authentication_permission TEXT NOT NULL, me_refresh_token TEXT NOT NULL )`
+  db.run(sql);
 }
 
 module.exports = {
-    getData,
-    postData,
-    putData,
-    create
+  insert,
+  select,
+  update,
+  create
 }
