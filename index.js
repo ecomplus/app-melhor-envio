796a3589aef5c07f07c9489c0704e6636a0101b9
Talisson Ferreira <talissonf@gmail.com>
2018-10-24 03:48:10 -0300

    remove cart route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const routes = require('./src/routes')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -11,4 +11,5 @@ app.post('/callback', routes.callback.post)
 app.get('/callback', routes.callback.get)
 app.get('/redirect', routes.redirect.melhorenvio)
 app.post('/calculate', routes.calculate.post)
+app.post('/procedure/orders', routes.procedure.orders)
 app.listen(port)
