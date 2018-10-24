94de63d03b920f79fdec3acabf90a45d0ca2c9e8
Talisson Ferreira <talissonf@gmail.com>
2018-10-23 22:52:00 -0300

    add: melhor envio shipment cart

--- src/routes.js
+++ src/routes.js
@@ -34,8 +34,6 @@ let routes = {
       })
 
       me.auth.getToken(request.query.code, (body, res, err) => {
-        console.log(body)
-        console.log(res)
         if (err) {
           response.status(400)
           return response.send(err)
@@ -78,7 +76,6 @@ let routes = {
         short_description: 'After received order, update melhor envio cart.',
         triggers: [
           {
-            method: 'POST',
             resource: 'orders'
           }
         ],
@@ -93,14 +90,14 @@ let routes = {
           {
             api: {
               external_api: {
-                uri: 'https://ecomplus-melhor-envio.herokuapp.com/procedure'
+                uri: 'https://ecomplus-melhor-envio.herokuapp.com/procedure/orders'
               }
             },
             method: 'POST',
             send_body: true
           }
         ],
-        tag: 'new_order'
+        tag: 'melhor_envio_orders'
       }
 
       let options = {
@@ -123,7 +120,8 @@ let routes = {
         return response.end()
       })
     },
-    get: (request, response) => {
+    orders: (request, response) => {
+      orders()
     }
   },
   calculate: {
@@ -148,24 +146,30 @@ let routes = {
           me.auth.refreshToken(ret.me_refresh_token, (respBody, resp, erro) => {
             if (erro) {
               response.status(400)
-              return response.json(erro)
+              return response.send(erro)
             }
 
             dao.update({ me_refresh_token: respBody.refresh_token }, { store_id: request.headers['x-store-id'] })
             me.setToken = respBody.access_token
             me.shipment.calculate(shipmentBody, (respBody, resp, erro) => {
+              console.log(resp)
               if (erro) {
                 response.status(400)
-                return response.json(erro)
+                return response.send(erro)
               }
-              return response.json(respBody)
+              return response.send(JSON.stringify(respBody))
             })
           })
         })
       } catch (error) {
         response.status(400)
-        return response.json(error)
+        return response.send(error)
       }
+    }
+  },
+  cart: {
+    post: (request, response) => {
+      cart(request, response)
     }
   }
 }
@@ -241,6 +245,217 @@ let getEcomApiToken = (xStore, aId) => {
     body: { '_id': aId },
     json: true
   })
+}
+
+let orders = (request, response) => {
+  if (!request.body) {
+  }
+
+  dao.select({ store_id: request.headers['x-store-id'] }, (ret) => {
+    let orderId = request.body.resource_id
+    let options = {
+      method: 'GET',
+      uri: 'https://api.e-com.plus/v1/orders/' + orderId + '.json',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Store-ID': request.headers['x-store-id'],
+        'X-Access-Token': ret.ecom_at,
+        'X-My-ID': ret.authentication_id
+      }
+    }
+    rq.get(options, (erro, res) => {
+      if (erro) {
+        response.status(400)
+        return response.send('Failed E-com.plus API Request')
+      }
+      let order = res
+      if (order.fulfillment_status) {
+        switch (order.fulfillment_status.current) {
+          case 'ready_for_shipping':
+            cart(request, response)
+            break
+
+          default:
+            break
+        }
+      }
+      return response.end()
+    })
+  })
+}
+
+let cart = (request, response) => {
+  let shipmentBody = request.body
+
+  let me = new MelhorEnvio({
+    client_id: config.ME_CLIENT_ID,
+    client_secret: config.ME_CLIENT_SECRET,
+    sandbox: config.ME_SANDBOX,
+    redirect_uri: config.ME_REDIRECT_URI,
+    request_scope: config.ME_SCOPE
+  })
+
+  try {
+    dao.select({ store_id: request.headers['x-store-id'] }, (ret) => {
+      if (typeof ret === 'undefined') {
+        response.status(400)
+        return response.send('Token not found')
+      }
+      me.auth.refreshToken(ret.me_refresh_token, (respBody, resp, erro) => {
+        if (erro) {
+          response.status(400)
+          return response.send(erro)
+        }
+        dao.update({ me_refresh_token: respBody.refresh_token }, { store_id: request.headers['x-store-id'] })
+
+        me.setToken = respBody.access_token
+        me.user.cart(shipmentBody, (respBody, resp, erro) => {
+          if (erro) {
+            response.status(400)
+            return response.send(erro)
+          }
+
+          if (respBody.id) {
+            let cart = respBody._id
+            me.shipment.checkout(cart, (body, res, err) => {
+              if (err) {
+                response.status(400)
+                return response.send(err)
+              }
+
+              if (body.purchase) {
+                return response.send(body)
+              }
+            })
+          }
+        })
+      })
+    })
+  } catch (error) {
+    response.status(400)
+    return response.send(error)
+  }
+}
+
+let updateOrderStatus = (request, response) => {
+}
+
+let melhorEnvioCardSchema = (order, seller, service, agency) => {
+  let schema = {
+    'service': service,
+    'agency': agency,
+    'from': {
+      'name': seller.firstname + seller.lastname,
+      'phone': seller.phone.phone,
+      'email': seller.email, // email (opcional)
+      'document': seller.document, // cpf (opcional)
+      // 'company_document': '89794131000100', // cnpj (obrigatório se não for Correios)
+      // 'state_register': '123456', // inscrição estadual (obrigatório se não for Correios) pode ser informado 'isento'
+      'address': seller.address.address,
+      'complement': seller.address.complement,
+      'number': seller.address.number,
+      'district': seller.address.district,
+      'city': seller.address.city.city,
+      'state_abbr': seller.address.state.state_abbr,
+      'country_id': seller.address.city.state.country.id,
+      'postal_code': seller.address.postal_code
+    },
+    'to': { // destinatário
+      'name': order.shipping_lines[0].to.name,
+      'phone': order.shipping_lines[0].to.phone.number, // telefone com ddd (obrigatório se não for Correios)
+      'email': order.buyers[0].main_email,
+      'document': order.buyers[0].doc_number, // obrigatório se for transportadora e não for logística reversa
+      // 'company_document': '89794131000100', // (opcional) (a menos que seja transportadora e logística reversa)
+      // 'state_register': '123456', // (opcional) (a menos que seja transportadora e logística reversa)
+      'address': order.shipping_lines[0].to.street,
+      'complement': order.shipping_lines[0].to.complement,
+      'number': order.shipping_lines[0].to.number,
+      'district': order.shipping_lines[0].to.borough,
+      'city': order.shipping_lines[0].to.city,
+      'state_abbr': order.shipping_lines[0].to.province_code,
+      'country_id': order.shipping_lines[0].to.country_code,
+      'postal_code': order.shipping_lines[0].to.zip,
+      'note': order.shipping_lines[0].to.near_to // (opcional) impresso na etiqueta
+    },
+    'products': [ // lista de produtos para preenchimento da declaração de conteúdo
+      {
+        'name': order.items.name, // nome do produto (max 255 caracteres)
+        'quantity': order.items.quantity, // quantidade de items desse produto
+        'unitary_value': order.items.price // R$ 4,50 valor do produto
+      }
+    ],
+    'package': { // informações do pacote (volume) - ainda não é possível cadastrar volumes
+      'weight': order.shipping_lines[0].package.weight,
+      'width': order.shipping_lines[0].package.dimensions.width.value,
+      'height': order.shipping_lines[0].package.dimensions.height.value,
+      'length': order.shipping_lines[0].package.dimensions.length.value
+    },
+    'options': { // opções
+      'insurance_value': order.shipping_lines[0].declared_value, // valor declarado/segurado
+      'receipt': false, // aviso de recebimento
+      'own_hand': false, // mão pŕopria
+      'collect': false, // coleta
+      'reverse': false, // logística reversa (se for reversa = true, ainda sim from será o remetente e to o destinatário)
+      'non_commercial': false, // envio de objeto não comercializável (flexibiliza a necessidade de pessoas júridicas para envios com transportadoras como Latam Cargo, porém se for um envio comercializável a mercadoria pode ser confisca pelo fisco)
+      'invoice': { // nota fiscal (opcional se for Correios)
+        'number': '12345', // número da nota
+        'key': 'nf-e' // chave da nf-e
+      }
+    }
+  }
+}
+
+let order = {
+  from: (seller) => {
+    return {
+      'name': seller.firstname + seller.lastname,
+      'phone': seller.phone.phone,
+      'email': seller.email, // email (opcional)
+      'document': seller.document, // cpf (opcional)
+      // 'company_document': '89794131000100', // cnpj (obrigatório se não for Correios)
+      // 'state_register': '123456', // inscrição estadual (obrigatório se não for Correios) pode ser informado 'isento'
+      'address': seller.address.address,
+      'complement': seller.address.complement,
+      'number': seller.address.number,
+      'district': seller.address.district,
+      'city': seller.address.city.city,
+      'state_abbr': seller.address.state.state_abbr,
+      'country_id': seller.address.city.state.country.id,
+      'postal_code': seller.address.postal_code
+    }
+  },
+  to: (order) => {
+    return { // destinatário
+      'name': order.shipping_lines[0].to.name,
+      'phone': order.shipping_lines[0].to.phone.number, // telefone com ddd (obrigatório se não for Correios)
+      'email': order.buyers[0].main_email,
+      'document': order.buyers[0].doc_number, // obrigatório se for transportadora e não for logística reversa
+      // 'company_document': '89794131000100', // (opcional) (a menos que seja transportadora e logística reversa)
+      // 'state_register': '123456', // (opcional) (a menos que seja transportadora e logística reversa)
+      'address': order.shipping_lines[0].to.street,
+      'complement': order.shipping_lines[0].to.complement,
+      'number': order.shipping_lines[0].to.number,
+      'district': order.shipping_lines[0].to.borough,
+      'city': order.shipping_lines[0].to.city,
+      'state_abbr': order.shipping_lines[0].to.province_code,
+      'country_id': order.shipping_lines[0].to.country_code,
+      'postal_code': order.shipping_lines[0].to.zip,
+      'note': order.shipping_lines[0].to.near_to // (opcional) impresso na etiqueta
+    }
+  },
+  products: (order) => {
+    let products = {}
+    products = {
+      'name': order.items.name, // nome do produto (max 255 caracteres)
+      'quantity': order.items.quantity, // quantidade de items desse produto
+      'unitary_value': order.items.price // R$ 4,50 valor do produto
+    }
+  },
+  package: (order) => {
+
+  },
+  options: (order) => {
+  }
 }
 
 module.exports = routes
