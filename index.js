a937667a6de63144f3bcd28c021efb2f8d736a39
Talisson Ferreira <talissonf@gmail.com>
2018-10-25 10:07:29 -0300

    Exceptions handle

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ const routes = require('./src/routes')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+process.on('uncaughtException', (err) => {
+  // fatal error
+  // log to file before exit
+  let msg = '\n[' + new Date().toString() + ']\n'
+  if (err) {
+    if (err.hasOwnProperty('stack')) {
+      msg += err.stack
+    } else if (err.hasOwnProperty('message')) {
+      msg += err.message
+    } else {
+      msg += err.toString()
+    }
+    msg += '\n'
+  }
+
+  let fs = require('fs')
+  fs.appendFile('/_stderr', msg, () => {
+    process.exit(1)
+  })
+})
+
 app.post('/callback', routes.callback.post)
 app.get('/callback', routes.callback.get)
 app.get('/redirect', routes.redirect.melhorenvio)
