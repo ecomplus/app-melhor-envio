0d4e374b203913d4dae7789832f28bcb36a3c949
Talisson Ferreira <talissonf@gmail.com>
2018-11-20 17:24:43 -0200

    Debug

--- src/melhorenvio/melhorenvio.js
+++ src/melhorenvio/melhorenvio.js
@@ -218,7 +218,7 @@ class MelhorEnvioApp {
   }
 
   async calculate (payload, xstoreId) {
-    // console.log(payload)
+    console.log(payload)
     console.log(typeof payload.params)
     return new Promise(async (resolve, reject) => {
       let meTokens = await this.getAppinfor(xstoreId)
