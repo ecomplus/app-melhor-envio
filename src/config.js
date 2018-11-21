184f2246588150ef0859d5272baa0cd95681885a
Leonardo Matos <leomp120894@gmail.com>
2018-11-21 15:03:01 -0200

    preset me scope

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ module.exports = {
   ME_CLIENT_SECRET: env.ME_CLIENT_SECRET,
   ME_SANDBOX: sandbox,
   ME_REDIRECT_URI: env.ME_REDIRECT_URI,
-  ME_SCOPE: env.ME_SCOPE,
+  ME_SCOPE: 'cart-read cart-write companies-read coupons-read notifications-read products-read products-write purchases-read shipping-calculate shipping-cancel shipping-checkout shipping-companies shipping-generate shipping-preview shipping-print shipping-share shipping-tracking ecommerce-shipping transactions-read users-read webhooks-read webhooks-write',
   TABLE_NAME: env.TABLE_NAME,
   BD_PATH: env.BD_PATH
 }
