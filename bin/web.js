e20815bcac0cbcf318d9ed4a31903204591e6296
Talisson Ferreira <talissonf@gmail.com>
2019-08-11 22:53:25 -0300

    revert: removed mocked vars

--- bin/web.js
+++ bin/web.js
@@ -7,12 +7,13 @@ const logger = require('console-files')
 // handle app authentication to Store API
 // https://github.com/ecomclub/ecomplus-app-sdk
 const { ecomAuth, ecomServerIps } = require('ecomplus-app-sdk')
+
 // melhorenvio-sdk https://github.com/talissonf/melhor-envio-sdk#instance
 const me = require('melhor-envio').config({
-  client_id: 31, //process.env.ME_CLIENT_ID,
-  client_secret: 'KYaPyv6odJuPvSbBg8TdIg4EErK4gWNDD5rE8oqU', //process.env.ME_CLIENT_SECRET,
+  client_id: process.env.ME_CLIENT_ID,
+  client_secret: process.env.ME_CLIENT_SECRET,
   sandbox: (process.env.ME_SANDBOX === 'true'),
-  redirect_uri: 'https://melhorenvio.ecomplus.biz/callback',//process.env.ME_REDIRECT_URI,
+  redirect_uri: process.env.ME_REDIRECT_URI,
   request_scope: `cart-read cart-write companies-read coupons-read notifications-read products-read products-write purchases-read shipping-calculate shipping-cancel shipping-checkout shipping-companies shipping-generate shipping-preview shipping-print shipping-share shipping-tracking ecommerce-shipping transactions-read users-read webhooks-read webhooks-write`
 })
 
@@ -21,26 +22,34 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // E-Com Plus Store ID from request header
 app.use((req, res, next) => {
-  if (req.originalUrl.startsWith('/ecom/') && process.env.NODE_ENV === 'production') {
-    // check if request is comming from E-Com Plus servers
-    if (ecomServerIps.indexOf(req.get('x-real-ip')) === -1) {
-      res.status(403).send('Who are you? Unauthorized IP address')
-    } else {
-      // get store ID from request header
-      req.storeId = parseInt(req.get('x-store-id'), 10)
-      next()
+  if (req.url.startsWith('/ecom/')) {
+    // get E-Com Plus Store ID from request header
+    req.storeId = parseInt(req.get('x-store-id'), 10)
+    if (req.url.startsWith('/ecom/modules/')) {
+      // request from Mods API
+      // https://github.com/ecomclub/modules-api
+      const { body } = req
+      if (typeof body !== 'object' || body === null || !body.params || !body.application) {
+        return res.status(406).send('Request not comming from Mods API? Invalid body')
+      }
     }
-  } else {
-    // bypass
-    next()
+
+    // on production check if request is comming from E-Com Plus servers
+    if (process.env.NODE_ENV === 'production' && ecomServerIps.indexOf(req.get('x-real-ip')) === -1) {
+      return res.status(403).send('Who are you? Unauthorized IP address')
+    }
   }
+
+  // pass to the endpoint handler
+  // next Express middleware
+  next()
 })
 
 ecomAuth.then(appSdk => {
@@ -63,7 +72,7 @@ ecomAuth.then(appSdk => {
   // debug
   router.get('/redirect', require('./../routes/melhorenvio/request-auth')(me))
   router.get('/callback', require('./../routes/melhorenvio/auth-callback')(me))
-  
+
   // add router and start web server
   app.use(router)
   app.listen(port)
