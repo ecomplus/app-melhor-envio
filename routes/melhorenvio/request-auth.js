31fbf234ed8dc674f1f721ee08ed431ee9a1e136
Talisson Ferreira <talissonf@gmail.com>
2020-07-02 23:41:53 -0300

    refactor: removed unsed files

--- routes/melhorenvio/request-auth.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = (me) => {
-  return (req, res) => {
-    // request oauth
-    let to = me.auth.getAuth() + '&state=' + req.query.x_store_id
-    return res.redirect(301, to)
-  }
-}
